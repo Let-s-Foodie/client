82833d5e1b2d0a4155485c3bad73cb402afdf527
Geonhyeong Park <Abby@Geonhyeongs-MacBook-Air.local>
2021-05-12 21:50:27 -0700

    change aux filename

--- src/component/RandomFood.js
+++ src/component/RandomFood.js
@@ -1,6 +1,6 @@
 import React,{useState} from "react";
 import axios from 'axios';
-import Aux from '../component/hoc/Aux';
+import Aux from './hoc/hoc';
 import DetailPage from './DetailPage/DetailPage';
 const RandomFood = ({ title, image,category,area }) => {
   const [details,setDetail] = useState([]);
