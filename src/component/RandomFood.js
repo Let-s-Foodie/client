af2396c1a21f0eef1934bfd7eba3e342bc41e3d7
Joon <jong@JONGs-MacBook-Pro.local>
2021-05-18 21:12:13 -0700

    n

--- src/component/RandomFood.js
+++ src/component/RandomFood.js
@@ -1,48 +1,46 @@
-import React,{useState} from "react";
-import axios from 'axios';
-import Aux from './hoc/hoc';
-import DetailPage from './DetailPage/DetailPage';
-const RandomFood = ({ title, image,category,area,lat,lng }) => {
-  const [details,setDetail] = useState([]);
+import React, { useState } from "react";
+import axios from "axios";
+import Aux from "./hoc/hoc";
+import DetailPage from "./DetailPage/DetailPage";
+const RandomFood = ({ title, image, category, area, lat, lng }) => {
+  const [details, setDetail] = useState([]);
   const detailHandler = () => {
-      
-      const URL = "http://localhost:5000/random/detail";
-      const data = {
-        'title':title,
-        'image':image,
-        'category':category,
-        'area':area,
-        'lat':lat,
-        'lng':lng
-      }
-      fetch(URL,{
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-          'Accept': 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
+    const URL = "http://localhost:5000/random/detail";
+    const data = {
+      title: title,
+      image: image,
+      category: category,
+      area: area,
+      lat: lat,
+      lng: lng,
+    };
+    fetch(URL, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+    })
+      .then(
+        (res) => {
+          return res.json();
         },
-      }) 
-      .then((res)=>{
-        return res.json();
-      },(error) => {
-        console.log(error);
-      })
-      .then(resData => {
-        console.log(resData.data.businesses)
-        setDetail(resData.data.businesses)
-      })
-  }
+        (error) => {
+          console.log(error);
+        }
+      )
+      .then((resData) => {
+        console.log(resData.data.businesses);
+        setDetail(resData.data.businesses);
+      });
+  };
   return (
     <Aux>
       <h2>{title}</h2>
-      <img 
-        src={image} 
-        alt={""} 
-        onClick={detailHandler}
-        />
-      {details.map(detail => (
-        <DetailPage 
+      <img src={image} alt={""} onClick={detailHandler} />
+      {details.map((detail) => (
+        <DetailPage
           name={detail.name}
           image={detail.image_url}
           key={detail.id}
