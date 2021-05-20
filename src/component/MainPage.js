d3df16be7c4e5e47f88c1e5871a1afbdc0852f4d
Joon <jong@jongs-mbp.lan>
2021-05-20 16:39:56 -0700

    hardcoding listing favorite foods

--- src/component/MainPage.js
+++ src/component/MainPage.js
@@ -1,33 +1,26 @@
 import React, { useState, useEffect } from "react";
 import RandomFoods from "../component/RandomFoods";
-import axios from 'axios';
+import axios from "axios";
 import useGeoLocation from "./hooks/useGeoLocation";
 import * as dataSet from "./Data/categories.json";
 
 const MainPage = () => {
- 
   const [foods, setFoods] = useState([]);
   const [foodInfo, setFoodInfo] = useState([]);
 
-  const [foodCategory,setCategory] = useState([]);
+  const [foodCategory, setCategory] = useState([]);
   useEffect(() => {
-     
-      
-      let dataset = dataSet.default.map(function(data){
-       
-        if(data.parents[0] === "restaurants" || data.parents[0] === "food"){
-            return data;
-        }
-        
-      })
-      let data = dataset.filter(function(el){
-        return el != null;
-      })
-      console.log(data)
-      setCategory(data);
-      
-  },[]);
-
+    let dataset = dataSet.default.map((data) => {
+      if (data.parents[0] === "restaurants" || data.parents[0] === "food") {
+        return data;
+      }
+    });
+    let data = dataset.filter((el) => {
+      return el != null;
+    });
+    console.log(data);
+    setCategory(data);
+  }, []);
 
   const {
     loaded,
@@ -35,33 +28,34 @@ const MainPage = () => {
   } = useGeoLocation();
 
   const categoriesHandler = () => {
-    let foodList =[];
+    let foodList = [];
     let i = foodCategory.length,
-        j = 0;
-       j = Math.floor(Math.random() * (i+1));
-       foodList.push(foodCategory[j]);
-       foodCategory.splice(j,1);
-       setCategory(foodCategory);
-       const foodLists = {
-         title: foodList[0].title,
-         
-        
-         
-       }
-       setFoods([foodLists])
-      
-       
-   
-  }
- 
- 
+      j = 0;
+    j = Math.floor(Math.random() * (i + 1));
+    foodList.push(foodCategory[j]);
+    foodCategory.splice(j, 1);
+    setCategory(foodCategory);
+    const foodLists = {
+      title: foodList[0].title,
+    };
+    setFoods([foodLists]);
+  };
+
   return (
     <div className='main-page'>
       <h1>Google Food</h1>
-  
-      {loaded && <button onClick={categoriesHandler}>Search</button>}
-      
-      {foods.length > 0 ? <RandomFoods foods={foods} lat={lat} lng={lng} /> : <div>GOOD LUCK</div>}
+
+      {loaded && (
+        <button className='' onClick={categoriesHandler}>
+          Search
+        </button>
+      )}
+
+      {foods.length > 0 ? (
+        <RandomFoods foods={foods} lat={lat} lng={lng} />
+      ) : (
+        <div>GOOD LUCK</div>
+      )}
       <div>
         {loaded ? (
           <>
