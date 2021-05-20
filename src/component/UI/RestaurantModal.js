a80e2093175cbe3606a7b13cfb0a547537414a1d
Geonhyeong Park <Abby@Geonhyeongs-MacBook-Air.local>
2021-05-19 19:47:38 -0700

    create Portal for backdrop, Modal

--- src/component/UI/RestaurantModal.js
+++ src/component/UI/RestaurantModal.js
@@ -1,27 +1,42 @@
-import react from 'react';
+import React from 'react';
+import ReactDom from 'react-dom';
 import "./RestaurantModal.css";
 import Hoc from '../hoc/hoc';
 import DetailPage from '../DetailPage/DetailPage';
+const Backdrop = ({onCancle}) => {
+    return  <div className="backdrop" onClick={onCancle} />;
+}
+const ModalOverlay = ({lat,lng,details}) => {
+    return (
+
+        <div className="modal">
+        <div className="content">
+            {details.map((detail) => (
+                <DetailPage 
+                    name={detail.name}
+                    image={detail.image_url}
+                    key={detail.id}
+                    lat={lat}
+                    lng={lng}
+                />
+            ))}
+        </div>
+  
+    </div>
+    )
+}
 const RestaurantModal = ({lat,lng,details, onCancle}) => {
     return (
         <Hoc>
-            <div className="backdrop" onClick={onCancle} />
+           
             
-        
-            <div className="modal">
-                <div className="content">
-                    {details.map((detail) => (
-                        <DetailPage 
-                            name={detail.name}
-                            image={detail.image_url}
-                            key={detail.id}
-                            lat={lat}
-                            lng={lng}
-                        />
-                    ))}
-                </div>
-          
-            </div>
+            {ReactDom.createPortal(
+                <Backdrop onCancle={onCancle}/>,
+                 document.getElementById('backdrop-root'))}
+            {ReactDom.createPortal(
+                <ModalOverlay lat ={lat} lng={lng} details={details}/>,
+                document.getElementById('overlay-root')
+            )}
         </Hoc>
     );
 }
