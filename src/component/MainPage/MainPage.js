80d3043c43f0ec22ccf01b37e66f414d266d875a
Geonhyeong Park <Abby@Geonhyeongs-MacBook-Air.local>
2021-05-23 18:47:20 -0700

    accept incoming changes

--- src/component/MainPage/MainPage.js
+++ src/component/MainPage/MainPage.js
@@ -13,7 +13,7 @@ const MainPage = () => {
 
   const [foodCategory, setCategory] = useState([]);
   useEffect(() => {
-<<<<<<< HEAD:src/component/MainPage.js
+
     let dataset = dataSet.default.map((data) => {
       if (data.parents[0] === "restaurants" || data.parents[0] === "food") {
         return data;
@@ -25,25 +25,9 @@ const MainPage = () => {
     console.log(data);
     setCategory(data);
   }, []);
-=======
-     
-      
-      let dataset = dataSet.default.map(function(data){
-       
-        if(data.parents[0] === "restaurants" || data.parents[0] === "food"){
-            return data;
-        }
-        
-      })
-      let data = dataset.filter(function(el){
-        return el != null;
-      })
-   
-      setCategory(data);
-      
-  },[]);
 
->>>>>>> main:src/component/MainPage/MainPage.js
+   
+
 
   const {
     loaded,
@@ -52,10 +36,8 @@ const MainPage = () => {
 
   const categoriesHandler = () => {
     let foodList = [];
-    let i = foodCategory.length,
-<<<<<<< HEAD:src/component/MainPage.js
-      j = 0;
-    j = Math.floor(Math.random() * (i + 1));
+    let i = foodCategory.length;
+    let j = Math.floor(Math.random() * (i + 1));
     foodList.push(foodCategory[j]);
     foodCategory.splice(j, 1);
     setCategory(foodCategory);
@@ -65,49 +47,7 @@ const MainPage = () => {
     setFoods([foodLists]);
   };
 
-  return (
-    <div className='main-page'>
-      <h1>Google Food</h1>
-
-      {loaded && (
-        <button className='' onClick={categoriesHandler}>
-          Search
-        </button>
-      )}
-
-      {foods.length > 0 ? (
-        <RandomFoods foods={foods} lat={lat} lng={lng} />
-      ) : (
-        <div>GOOD LUCK</div>
-      )}
-      <div>
-        {loaded ? (
-          <>
-            <p>Your Latitude: {lat}</p>
-            <p>Your Longitude: {lng}</p>
-          </>
-        ) : (
-          "Location data not available yet"
-        )}
-      </div>
-    </div>
-=======
-        j = 0;
-       j = Math.floor(Math.random() * (i+1));
-       foodList.push(foodCategory[j]);
-       foodCategory.splice(j,1);
-       setCategory(foodCategory);
-       
-       const foodLists = {
-         title: foodList[0].title,
-          image:""         
-       }
-       
-       setFoods([foodLists])
-      
-       
-   
-  }
+  
  
  
   return (
@@ -135,8 +75,7 @@ const MainPage = () => {
      </Aux>
      
      
-    
->>>>>>> main:src/component/MainPage/MainPage.js
+  
   );
 };
 
