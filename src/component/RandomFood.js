0fff07c8db0687aa548737494c2df0a3cc131547
Joon <jong@jongs-mbp.lan>
2021-05-23 18:16:07 -0700

    ready

--- src/component/RandomFood.js
+++ src/component/RandomFood.js
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
+import RestaurantModal from "../component/UI/RestaurantModal";
 import axios from "axios";
 import Aux from "./hoc/hoc";
-import DetailPage from "./DetailPage/DetailPage";
 const RandomFood = ({ title, lat, lng }) => {
   const [details, setDetail] = useState([]);
-=======
-import React,{useState} from "react";
-import RestaurantModal from "../component/UI/RestaurantModal";
-import axios from 'axios';
-import Aux from './hoc/hoc';
-const RandomFood = ({ title,lat,lng }) => {
- 
-  
-  const [details,setDetail] = useState([]);
 
->>>>>>> baa3028f6379591e8024105e049c493ffacc973d
   const detailHandler = () => {
     const URL = "http://localhost:5000/random/detail";
     const data = {
@@ -31,11 +20,13 @@ const RandomFood = ({ title,lat,lng }) => {
         "Content-Type": "application/json",
       },
     })
+      .then((res) => {
+        return res.json();
+      })
       .then(
         (res) => {
           return res.json();
         },
-<<<<<<< HEAD
         (error) => {
           console.log(error);
         }
@@ -44,47 +35,22 @@ const RandomFood = ({ title,lat,lng }) => {
         setDetail(resData.data.businesses);
       });
   };
+  const cancleHandler = () => {
+    setDetail([]);
+  };
+
   return (
     <Aux>
       <h2 onClick={detailHandler}>{title}</h2>
 
-      {details.length > 0 &&
-        details.map((detail) => (
-          <DetailPage
-            name={detail.name}
-            image={detail.image_url}
-            key={detail.id}
-            lat={lat}
-            lng={lng}
-          />
-        ))}
-=======
-      }) 
-      .then((res)=>{
-        return res.json();
-      },(error) => {
-        console.log(error);
-      })
-      .then(resData => {
-        setDetail(resData.data.businesses)
-      })
-  }
-  const cancleHandler = () => {
-    setDetail([]);
-  }
-  
-  return (
-    <Aux>
-      <h2 onClick={detailHandler}>{title}</h2>
-     
-      {details.length > 0 && 
-      <RestaurantModal 
-        lat={lat}
-        lng={lng}
-        details={details}
-        onCancle={cancleHandler}
-      />} 
->>>>>>> baa3028f6379591e8024105e049c493ffacc973d
+      {details.length > 0 && (
+        <RestaurantModal
+          lat={lat}
+          lng={lng}
+          details={details}
+          onCancle={cancleHandler}
+        />
+      )}
     </Aux>
   );
 };
