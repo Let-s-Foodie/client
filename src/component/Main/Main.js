100611769c9ba1ae12ee25a3ec535fdc8b5c81a8
pg0817h <pg0817h@gmail.com>
2021-10-20 22:45:11 -0700

    main.js_refactoring

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -3,36 +3,44 @@ import RandomFood from "../RandomFood/RandomFood";
 import useGeoLocation from "../hooks/useGeoLocation";
 import Aux from '../hoc/hoc';
 import "./Main.css";
-const Main = () => {
+const Main = ({dishLists,location}) => {
 
   const [foodFeed, setFoodfeed] = useState([]);
   const [foodInfo, setFoodinfo] = useState([]);
   const [lengthCheck, setLength] = useState([]);
 
-  useEffect(() => {
+  // useEffect(() => {
    
-    //const URL = "http://localhost:8000/dishes";
-    const URL ="http://localhost:8000/random/feeds"
-    fetch(URL, {
-      method: 'GET',
+  //   //const URL = "http://localhost:8000/yelp/dishes";
+  //   const URL ="http://localhost:8000/random/feeds"
+  //   fetch(URL, {
+  //     method: 'GET',
 
-    })
-    .then((res)=>{
-      console.log(res)
-      return res.json()})
-    .then((data)=> {
+  //   })
+  //   .then((res)=>{
+  //     console.log(res)
+  //     return res.json()})
+  //   .then((data)=> {
 
-    console.log(data)
-    setFoodinfo(data);
-    if(foodInfo.length <= 1) setLength("loading")
-    })
-    return () => {
-      setFoodinfo({});
-      setLength();
-    }
+  //   console.log(data)
+  //   setFoodinfo(data);
+  //   if(foodInfo.length <= 1) setLength("loading")
+  //   })
+  //   return () => {
+  //     setFoodinfo({});
+  //     setLength();
+  //   }
   
-  },[lengthCheck,foodInfo.length])
-
+  // },[lengthCheck,foodInfo.length])
+  useEffect(()=>{
+    setFoodinfo(dishLists)
+    if(foodInfo.length <= 1) setLength("loading");
+  
+    // clean up
+    //return () => {
+    //   setFoodinfo([])
+    // }
+  },[dishLists,lengthCheck,foodInfo.length])
   const {
     loaded,
     coordinates: { lat, lng },
@@ -40,51 +48,54 @@ const Main = () => {
 
  
   const foodListHandler = () => {
-    let foodArr = [];
+    //let foodArr = [];
+    let randomItem;
     let i = foodInfo.length;
     console.log("FoodInfo length test",foodInfo.length)
     let j = Math.floor(Math.random() * (i));
-    foodArr.push(foodInfo[j]);
+    //foodArr.push(foodInfo[j]);
+    randomItem = foodInfo[j];
     foodInfo.splice(j,1);
     setFoodinfo(foodInfo);
     const foods = {
-      title: foodArr[0].name,
-      image: foodArr[0].image,
-      category: foodArr[0].category,
-      seller: foodArr[0].sellerId,
-      id:foodArr[0].id
+      title: randomItem.name,
+      image: randomItem.image,
+      category: randomItem.category,
+      seller: randomItem.sellerId,
+      id: randomItem.id
     };
    
    
     setFoodfeed(foods);
+    console.log("----------------------foodFeed1", foodFeed)
     
   }
- 
+ console.log("----------------------foodFeed2", foodFeed)
  
   return (
    
      <Aux>
       <section className="main-page">
-      {!loaded && <div>Loading...</div>}
+      { foodInfo.length === 0 && <div>Loading...</div>}
      
    
       
-    { loaded && !foodFeed.title && <button onClick={foodListHandler}>Test button</button>   }
+    { foodInfo.length > 0 && !foodFeed.title && <button onClick={foodListHandler}>Test button</button>   }
      
     
 
-          {foodFeed.title?
+          {foodFeed.title &&
             <RandomFood
               title = {foodFeed.title} 
               image={foodFeed.image}
-              lat={lat}
-              lng={lng}
+              lat={location.lat}
+              lng={location.lng}
               randomHandler={foodListHandler}
               seller={foodFeed.seller}
               category={foodFeed.category}
               
               />
-            : <div></div>}
+          }
 
       </section>
       
