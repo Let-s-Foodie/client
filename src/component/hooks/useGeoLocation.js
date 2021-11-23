1cbf5a6d7dc97c8a1dc3d9a04951b0b575f865fb
Joon <sunkist5691@gmail.com>
2021-11-22 22:36:18 -0800

    ready

--- src/component/hooks/useGeoLocation.js
+++ src/component/hooks/useGeoLocation.js
@@ -1,33 +1,39 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from 'react'
 
 const useGeoLocation = () => {
   const [location, setLocation] = useState({
     loaded: false,
-    coordinates: { lat: "", lng: "" },
-  });
-  
-  const onSuccess = (position) => {
-    const { longitude, latitude } = position.coords;
-    setLocation({
-      ...location,
-      loaded: true,
-      coordinates: { lat: latitude, lng: longitude },
-    });
-  };
+    coordinates: { lat: '', lng: '' },
+  })
 
-  const onError = (error) => {
-    setLocation({
-      ...location,
-      loaded: true,
-      err: error,
-    });
-  };
+  const onSuccess = useCallback(
+    (position) => {
+      const { longitude, latitude } = position.coords
+      setLocation({
+        ...location,
+        loaded: true,
+        coordinates: { lat: latitude, lng: longitude },
+      })
+    },
+    [location],
+  )
+
+  const onError = useCallback(
+    (error) => {
+      setLocation({
+        ...location,
+        loaded: true,
+        err: error,
+      })
+    },
+    [location],
+  )
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(onSuccess, onError);
-  }, []);
+    navigator.geolocation.getCurrentPosition(onSuccess, onError)
+  }, [onError, onSuccess])
 
-  return location;
-};
+  return location
+}
 
-export default useGeoLocation;
+export default useGeoLocation
