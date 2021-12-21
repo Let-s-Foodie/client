ded823325b5176df6e0995b45291bb4d7f9f32f5
Joon <sunkist5691@gmail.com>
2021-12-20 23:00:56 -0800

    fix private router

--- src/pages/SellerRoute/SellerRoute.jsx
+++ src/pages/SellerRoute/SellerRoute.jsx
@@ -1,26 +1,24 @@
-import React, { useContext } from "react";
-import AuthContext from "../../store/auth-context";
-import { Redirect } from "react-router-dom";
-import SellerRegisterPage from "../SellerRegisterPage";
-import { Route } from "react-router-dom";
-const SellerRoute = ({ component: Component, loaded }) => {
-
-  const authCtx = useContext(AuthContext);
+import React, { useContext } from 'react'
+import AuthContext from '../../store/auth-context'
+import { Redirect } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+const SellerRoute = (rest) => {
+  const authCtx = useContext(AuthContext)
   const isLoading = () => {
-    if (!loaded) {
-      return <></>;
+    if (!rest.loaded) {
+      return <></>
     } else {
       if (authCtx.isLoggedIn && authCtx.isSeller) {
-        return <Component />;
+        return <Route {...rest} />
       } else if (authCtx.isLoggedIn && !authCtx.isSeller) {
-        return <Redirect to="/" />;
+        return <Redirect to="/" />
       } else {
-        return <Redirect to="/seller/home" />;
+        return <Redirect to="/seller/home" />
       }
     }
-  };
+  }
 
-  return <>{isLoading()}</>;
-};
+  return <>{isLoading()}</>
+}
 
-export default SellerRoute;
+export default SellerRoute
